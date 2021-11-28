cf0ded476a3723c11f5eddd3da59b957a6561d7e
rvanderwerf <rvanderwerf@gmail.com>
2021-11-28 11:24:00 -0600

    fix mqtt

--- lab2/inference_client/sample-client-camera-mqtt.py
+++ lab2/inference_client/sample-client-camera-mqtt.py
@@ -5,6 +5,19 @@ import cv2
 import grpc
 from inference_server_pb2_grpc import InferenceServerStub
 import inference_server_pb2 as pb2 
+import sys
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
+import time as t
+import json
+
+ENDPOINT = "aidnfuomgla6i-ats.iot.us-east-1.amazonaws.com"
+CLIENT_ID = "l4vJetsonXavierNx"
+PATH_TO_CERTIFICATE = "/greengrass/v2/thingCert.crt"
+PATH_TO_PRIVATE_KEY = "/greengrass/v2/privKey.key"
+PATH_TO_AMAZON_ROOT_CA_1 = "/greengrass/v2/rootCA.pem"
+TOPIC = "l4v/testclient"
+
 
 
 
@@ -59,8 +72,38 @@ if cap.isOpened():
                 ) 
             )
     )
+    
+    event_loop_group = io.EventLoopGroup(1)
+    host_resolver = io.DefaultHostResolver(event_loop_group)
+    client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+    mqtt_connection = mqtt_connection_builder.mtls_from_path(
+                        endpoint=ENDPOINT,
+                        cert_filepath=PATH_TO_CERTIFICATE,
+                        pri_key_filepath=PATH_TO_PRIVATE_KEY,
+                        client_bootstrap=client_bootstrap,
+                        ca_filepath=PATH_TO_AMAZON_ROOT_CA_1,
+                        client_id=CLIENT_ID,
+                        clean_session=False,
+                        keep_alive_secs=6
+                        )
+    print("Connecting to {} with client ID '{}'...".format(
+                ENDPOINT, CLIENT_ID))
+    # Make the connect() call
+    connect_future = mqtt_connection.connect()
+    # Future.result() waits until a result is available
+    connect_future.result()
+    print("Connected!")
+    # Publish message to server desired number of times.
+    print('Begin Publish')
+    data = "{} [{}]".format(str(response),1)
+    message = {"message" : data}
+    mqtt_connection.publish(topic=TOPIC, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
+    print("Published: '" + json.dumps(message) + "' to the topic: " + TOPIC)
+    t.sleep(0.1)
+    print('Publish End')
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
     print(response)
-
 
 else:
     print("Unable to open camera")
